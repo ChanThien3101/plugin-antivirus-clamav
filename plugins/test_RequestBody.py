82c4392550b20093cabbc6b385ed6a7072a51e68
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-12-27 23:19:53 +0700

    finishedd

--- plugins/test_RequestBody.py
+++ plugins/test_RequestBody.py
@@ -1,10 +1,10 @@
 import requests
 
 url = "http://<your-server>"
+
+#data eicar for ClamAV
 data = "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 response = requests.post(url, data=data, headers={"Content-Type": "text/plain"})
 print("Status Code:", response.status_code)
 print("Response Body:", response.text)
-
-
