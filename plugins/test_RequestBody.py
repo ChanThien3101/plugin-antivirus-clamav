d934af56fcd8ac0db2ca7eb6dc5b7cbebb08aefa
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-12-27 23:16:51 +0700

    finisheddđ

--- plugins/test_RequestBody.py
+++ plugins/test_RequestBody.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://192.168.1.144/index.php"
+url = "http://<your-server>"
 data = "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
 response = requests.post(url, data=data, headers={"Content-Type": "text/plain"})
